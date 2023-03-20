61b144c49becd383e1bb121aebbe331b49274b99
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-03-20 16:53:56 +0000

    Use 3rd pary middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) Middlewares
+
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) Route handlers
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -106,12 +112,16 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) Server
 
 const port = 3000;
 app.listen(port, () => {
